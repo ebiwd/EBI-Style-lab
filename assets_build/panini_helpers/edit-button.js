7d8f0c04d19c8994d6c59b54fa4aa1a1dcb93f44
khawkins98 <khawkins98@gmail.com>
2017-05-25 10:38:42 +0100

    Add  and improve edit links

--- assets_build/panini_helpers/edit-button.js
+++ assets_build/panini_helpers/edit-button.js
@@ -3,12 +3,14 @@ module.exports = function(path, type) {
   try {
     var githubpath = 'https://github.com/ebiwd/EBI-Style-lab/edit/master';
 
+    var classes = 'button small secondary edit-button float-right';
+
     if (type === 'section')
-      return '<a class="button small secondary edit-button" targe="_blank" href="' + githubpath + '/assets_site/partials/' + path + '">Edit ' + type + '</a>';
+      return '<a class="'+classes+'" targe="_blank" href="' + githubpath + '/assets_site/partials/' + path + '">Edit ' + type + '</a>';
     if (type === 'layout')
-      return '<a class="button small secondary edit-button" targe="_blank" href="' + githubpath + '/assets_site/layouts/' + path + '.html">Edit ' + type + '</a>';
+      return '<a class="'+classes+'" targe="_blank" href="' + githubpath + '/assets_site/layouts/' + path + '.html">Edit ' + type + '</a>';
     if (type === 'page')
-      return '<a class="button small secondary edit-button" targe="_blank" href="' + githubpath + '/assets_site/pages/' + path + '/index.html">Edit ' + type + '</a>';
+      return '<a class="'+classes+'" targe="_blank" href="' + githubpath + '/assets_site/pages/' + path + '/index.html">Edit ' + type + '</a>';
   }
   catch(ex) {
     return null;
