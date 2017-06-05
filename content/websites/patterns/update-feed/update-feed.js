2db8ee66702482c2cd24c18c68d584bdeaf16c08
khawkins98 <khawkins98@gmail.com>
2017-06-05 16:51:13 +0100

    Spruce up data pattern

--- content/websites/patterns/update-feed/update-feed.js
+++ content/websites/patterns/update-feed/update-feed.js
@@ -8,32 +8,28 @@ var feedTemplate = '{{#each item}}'+
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
     // feed processor
-    function HandlebaRss(feedUrl,template,destination){
+    function feedImporter(feedUrl,template,destination){
       this.feedUrl = feedUrl;
       this.template = template;
       this.destination = destination;
     }
-    HandlebaRss.prototype.init = function(){
+    feedImporter.prototype.init = function(){
       function queryData(properties) {
         $.ajax({
+          // here we use YQL to translate the RSS into JSON, but you could easily pass in your JSON here
           url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22'+ encodeURIComponent(properties.feedUrl)+'%22limit%206&&format=json&diagnostics=false&_maxage=3600',
           template:    properties.template,
           destination: properties.destination
         }).done( function(data) {
-          console.log(data);
-          if (data.query.results.item.length > 0) {
-            var template = Handlebars.compile(properties.template);
-            $(properties.destination).html(template(data.query.results));
-          } else {
-            // retry if 0 length
-            window.setTimeout(queryData(properties), 500);
-          }
+          // here you need to map your JSON structure
+          var mappedData = data.query.results,
+              template = Handlebars.compile(properties.template);
+          $(properties.destination).html(template(mappedData));
         });
       }
-
       queryData(this); // bootstrap
     }
 
-    newsUpdates =  new HandlebaRss("http://www.ebi.ac.uk/about/news/service.xml",feedTemplate, "#news-updates").init();
+    newsUpdates = new feedImporter("http://www.ebi.ac.uk/about/news/service.xml",feedTemplate, "#news-updates").init();
   }
 }
