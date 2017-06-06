4da5d672b11a97480bc39937969db4f3d000a1f1
khawkins98 <khawkins98@gmail.com>
2017-06-06 10:36:22 +0100

    Add large form pattern

--- content/websites/patterns/update-feed/update-feed.js
+++ content/websites/patterns/update-feed/update-feed.js
@@ -5,31 +5,26 @@ var feedTemplate = '{{#each item}}'+
     '</div>'+
   '{{/each}}';
 
-document.onreadystatechange = function () {
-  if (document.readyState === "complete") {
-    // feed processor
-    function feedImporter(feedUrl,template,destination){
-      this.feedUrl = feedUrl;
-      this.template = template;
-      this.destination = destination;
-    }
-    feedImporter.prototype.init = function(){
-      function queryData(properties) {
-        $.ajax({
-          // here we use YQL to translate the RSS into JSON, but you could easily pass in your JSON here
-          url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22'+ encodeURIComponent(properties.feedUrl)+'%22limit%206&&format=json&diagnostics=false&_maxage=3600',
-          template:    properties.template,
-          destination: properties.destination
-        }).done( function(data) {
-          // here you need to map your JSON structure
-          var mappedData = data.query.results,
-              template = Handlebars.compile(properties.template);
-          $(properties.destination).html(template(mappedData));
-        });
-      }
-      queryData(this); // bootstrap
-    }
-
-    newsUpdates = new feedImporter("http://www.ebi.ac.uk/about/news/service.xml",feedTemplate, "#news-updates").init();
-  }
+// feed processor
+function feedImporter(feedUrl,template,destination){
+  this.feedUrl = feedUrl;
+  this.template = template;
+  this.destination = destination;
 }
+feedImporter.prototype.init = function(){
+  function queryData(properties) {
+    $.ajax({
+      // here we use YQL to translate the RSS into JSON, but you could easily pass in your JSON here
+      url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22'+ encodeURIComponent(properties.feedUrl)+'%22limit%206&&format=json&diagnostics=false&_maxage=3600',
+      template:    properties.template,
+      destination: properties.destination
+    }).done( function(data) {
+      // here you need to map your JSON structure
+      var mappedData = data.query.results,
+          template = Handlebars.compile(properties.template);
+      $(properties.destination).html(template(mappedData));
+    });
+  }
+  queryData(this); // bootstrap
+}
+newsUpdates = new feedImporter("http://www.ebi.ac.uk/about/news/service.xml",feedTemplate, "#news-updates").init();
